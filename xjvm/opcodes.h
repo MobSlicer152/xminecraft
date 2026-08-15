af17c2260df657dc6d9458e2c81c87e25e746329
MobSlicer152 <brambleclaw1414@gmail.com>
2026-08-15 18:41:06 -0400

    [xjvm] Improve opcode table significantly

--- xjvm/opcodes.h
+++ xjvm/opcodes.h
@@ -1,4 +1,5 @@
 /// Yes, as a matter of fact, I did hand type these from the spec. This is because I am not smart.
+/// TODO: some of this needs refactoring to actually be useful, but the values are enough for now.
 
 #pragma once
 
@@ -17,8 +18,8 @@ using Opcode = uint8_t;
 /// </summary>
 struct OpcodeInfo
 {
-	Opcode code;
 	std::string_view name;
+	uint8_t size;
 };
 
 /// <summary>
@@ -27,12 +28,20 @@ struct OpcodeInfo
 namespace Opcodes
 {
 
-#define XJVM_OPCODE(name, val)                                \
-	static constexpr Opcode name = (val);                     \
-	static constexpr OpcodeInfo name##_INFO = {name, #name};
-
+#define XJVM_OPCODE(name, val, ...) static constexpr Opcode name = (val);
 #include "opcodes.inc"
+#undef XJVM_OPCODE
 
-} // namespace Opcode
+} // namespace Opcodes
+
+/// <summary>
+/// Opcode info table
+/// NOTE: opcodes.inc MUST stay sorted by value for this to be correct
+/// </summary>
+static constexpr OpcodeInfo OPCODE_INFO[] = {
+#define XJVM_OPCODE(name, val, size) {#name, size},
+#include "opcodes.inc"
+#undef XJVM_OPCODE
+};
 
 } // namespace XJVM
