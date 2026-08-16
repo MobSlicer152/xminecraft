e56fd820af923add43288c1b976feda9bb7775ec
MobSlicer152 <brambleclaw1414@gmail.com>
2026-08-15 21:08:23 -0400

    [xjvm] Handle wide prefix

--- xjvm/opcodes.h
+++ xjvm/opcodes.h
@@ -20,6 +20,7 @@ struct OpcodeInfo
 {
 	std::string_view name;
 	uint8_t size;
+	uint8_t wideSize;
 };
 
 /// <summary>
@@ -39,7 +40,7 @@ namespace Opcodes
 /// NOTE: opcodes.inc MUST stay sorted by value for this to be correct
 /// </summary>
 static constexpr OpcodeInfo OPCODE_INFO[] = {
-#define XJVM_OPCODE(name, val, size) {#name, size},
+#define XJVM_OPCODE(name, val, size, ...) {#name, size, __VA_ARGS__},
 #include "opcodes.inc"
 #undef XJVM_OPCODE
 };
