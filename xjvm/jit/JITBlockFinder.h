7876038d7aaec8627728712e7b1057ae74dbf467
MobSlicer152 <brambleclaw1414@gmail.com>
2026-08-19 15:29:07 -0400

    [xjvm] Begin designing JIT

---
+++ xjvm/jit/JITBlockFinder.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../InstructionReader.h"
+
+namespace XJVM
+{
+
+class JITBlockFinder: public IInstructionProcessor
+{
+};
+
+} // namespace XJVM
--- xjvm/xjvm.h
+++ xjvm/xjvm.h
@@ -6,13 +6,14 @@
 #pragma warning(disable : 4200, justification : "zero-length arrays are useful")
 
 #include "ClassFile.h"
-#include "class_structs.h"
 #include "FieldDescriptor.h"
-#include "hash.h"
 #include "InstructionReader.h"
 #include "JarFile.h"
-#include "jvmdef.h"
 #include "OffsetSpan.h"
+#include "class_structs.h"
+#include "hash.h"
+#include "jit/JITBlockFinder.h"
+#include "jvmdef.h"
 #include "util.h"
 
 #pragma warning(pop)
