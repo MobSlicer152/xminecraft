69b49c245b8d45751e7ef00ec7d309e5dd97ca71
MobSlicer152 <brambleclaw1414@gmail.com>
2026-08-19 15:27:41 -0400

    [jvmtest] Update to match new JarFile design

--- jvmtest/jvmtest.cpp
+++ jvmtest/jvmtest.cpp
@@ -23,28 +23,29 @@ int main(int argc, char* argv[])
 		return ERROR_INVALID_DATA;
 	}
 
-	for (const auto& [name, classFile] : jar.GetClasses())
+	for (const auto& name : jar.GetClasses())
 	{
-		std::println("class name is \"{}\"", classFile.GetString(classFile.GetThisClass()));
-		std::println("superclass name is \"{}\"", classFile.GetString(classFile.GetSuperClass()));
-		std::println("class version is {}.{}", classFile.GetMajorVersion(), classFile.GetMinorVersion());
+		auto classFile = jar.GetClass(name);
+		std::println("class name is \"{}\"", classFile->GetString(classFile->GetThisClass()));
+		std::println("superclass name is \"{}\"", classFile->GetString(classFile->GetSuperClass()));
+		std::println("class version is {}.{}", classFile->GetMajorVersion(), classFile->GetMinorVersion());
 
-		for (uint16_t i = 1; i < classFile.GetConstantPoolSize(); i++)
+		for (uint16_t i = 1; i < classFile->GetConstantPoolSize(); i++)
 		{
-			auto constant = classFile.GetConstant(i);
+			auto constant = classFile->GetConstant(i);
 			// std::println("constant {} has type {} and string \"{}\"", i, (XJVM::u1)constant.tag, test.GetString(constant));
 		}
 
-		for (size_t i = 0; i < classFile.GetInterfaceCount(); i++)
+		for (size_t i = 0; i < classFile->GetInterfaceCount(); i++)
 		{
-			std::println("interface {} is {}", i, classFile.GetInterface(i));
+			std::println("interface {} is {}", i, classFile->GetInterface(i));
 		}
 
-		auto methods = classFile.GetMethods();
+		auto methods = classFile->GetMethods();
 		for (const auto& method : methods)
 		{
 			std::println("{} -> {}", method.GetName(), method.GetDescriptor());
-			auto code = classFile.GetCode(method);
+			auto code = classFile->GetCode(method);
 			XJVM::InstructionReader reader(code);
 			XJVM::PrintInstructionProcessor proc;
 			if (reader.Parse(&proc) < code.size())
